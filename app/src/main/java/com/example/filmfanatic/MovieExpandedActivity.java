65fba81a0bb536f3f51eeddfd7febf95d4f5b83f
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2021-08-10 21:42:44 -0400

    Update MovieExpandedActivity.java

--- app/src/main/java/com/example/filmfanatic/MovieExpandedActivity.java
+++ app/src/main/java/com/example/filmfanatic/MovieExpandedActivity.java
@@ -111,7 +111,8 @@ public class MovieExpandedActivity extends YouTubeBaseActivity {
         Movie movie= Parcels.unwrap(getIntent().getParcelableExtra("movie"));
         tvTitle.setText(movie.getTitle());
         tvDescription.setText("OVERVIEW: " + movie.getOverview());
-        Movie_Cast_Info="https://api.themoviedb.org/3/movie/" + movie.getMovieId() +"/credits?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        Movie_Cast_Info="https://api.themoviedb.org/3/movie/" + movie.getMovieId() +"/credits?api_key="+getResources().getString(R.string.MovieDBAPIKey);
+
 
         btnSeeReviews.setOnClickListener(new View.OnClickListener() {
             @Override
